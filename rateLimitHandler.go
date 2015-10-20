c375cb04dcd95b30fd1aa7ccfde861e2124d7aac
Max Korenkov <mkorenkov@gmail.com>
2015-10-19 18:23:16 -0700

    http.Handler as a param

--- rateLimitHandler.go
+++ rateLimitHandler.go
@@ -9,7 +9,7 @@ type CheckExceedTheLimit func(clientIP string) bool
 
 // RateLimit throttles incoming HTTP requests per IP. Helps against DDoS and content scans.
 // http://stackoverflow.com/questions/20298220/rate-limiting-http-requests-via-http-handlerfunc-middleware
-func RateLimit(h http.HandlerFunc, validationFunc CheckExceedTheLimit) http.HandlerFunc {
+func RateLimit(h http.Handler, validationFunc CheckExceedTheLimit) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		remoteIP := r.Header.Get("REMOTE_ADDR")
 		if validationFunc(remoteIP) {
